b31d3c353a805550fce3c493d1962f76fa966cb0
Kamylle Victoria Santos <sharmankamylle@gmail.com>
2023-12-08 15:05:12 -0300

    first commit

---
+++ class1/main.js
@@ -0,0 +1,14 @@
+class Pearson{
+    constructor(p_name){
+        this.name = p_name;
+    }
+}
+let p1 = new Pearson('ana');
+let p2 = new Pearson('caio');
+let p3 = new Pearson('lucas');
+let p4 = new Pearson('pedro');
+
+
+console.log(p2.name)
+console.log(p3.name)
+console.log(p4.name)  
