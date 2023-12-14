aa7c49ca3178188a514ba027f206819d687dd506
Kamylle Victoria Santos <sharmankamylle@gmail.com>
2023-12-14 13:17:11 -0300

    inheritance in js, bug fix

--- aula6/main.js
+++ aula6/main.js
@@ -1,56 +1,57 @@
 class Car {
-    constructor(name, doors) {
+  constructor(name, doors) {
       this.name = name;
       this.doors = doors;
-      this.on = false; // Inicializado como false
-      this.velocity = 0; // Inicializado como 0
-      this.color = '';
-    }
-  
-    carOn() {
-      this.on = true;
-    }
-  
-    carOff() {
       this.on = false;
       this.velocity = 0;
-    }
-  
-    setColor(color) {
-      this.color = color;
-    }
-  
-    setVelocity(velocity) {
-      if (this.on) {
-        this.velocity = velocity;
-      } else {
-        console.log("O carro está desligado. Ligue o carro primeiro.");
-      }
-    }
+      this.color = '';
   }
-  
-  class Military extends Car {
-    constructor(name, doors, shield, ammunition) {
-      super(name, doors); //invoca instancias da classes pai
+
+  carOn() {
+      this.on = true;
+  }
+
+  carOff() {
+      this.on = false;
+      this.velocity = 0;
+  }
+
+  setColor(color) {
+      this.color = color;
+  }
+
+  setVelocity(velocity) {
+      this.on = true;
+      this.velocity = velocity;
+      if(velocity === 0 || velocity <= 0){
+        this.on = false;
+        this.velocity = velocity;
+      }
+  }
+}
+
+class Military extends Car {
+  constructor(name, doors, shield, ammunition) {
+      super(name, doors);
+      this.setColor("Black");
       this.shield = shield;
       this.ammunition = ammunition;
-      this.setColor("Black");
-    }
-  
-    shotDown() {
-      if (this.ammunition > 0) {
-        console.log("Go!");
-        this.ammunition--;
-      } else {
-        console.log("Stop!");
-      }
-    }
   }
-  
-  const car2 = new Military("Volvo", 3, "Yes", 50);
-  
-  car2.carOn(); // Ligar o carro
-  car2.setVelocity(50); // Definir a velocidade
-  
-  console.log(`Name: ${car2.name} \nDoors: ${car2.doors}\nColor: ${car2.color} \nOn: ${car2.on} \nVelocity: ${car2.velocity}`);
-  
+
+  setShield(shield) {
+      this.shield = shield;
+  }
+  setAmmunition(ammunition) {
+      this.ammunition = ammunition;
+  }
+}
+
+const car2 = new Military("Volvo", 3);
+car2.setVelocity(120);
+car2.setAmmunition(12)
+car2.setShield(32)
+
+// Imprimindo todas as propriedades de car2
+console.log(`Basic car informations: Name: ${car2.name} \nDoors: ${car2.doors} \nOn: ${car2.on} \nVelocity: ${car2.velocity} \nColor: ${car2.color}`);
+console.log('-------------');
+console.log(`Military informations: \nShield: ${car2.shield} \nAmmunition: ${car2.ammunition}`)
