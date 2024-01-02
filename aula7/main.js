c15de26cbc86c63323b541626f4e05832397c5a4
Kamylle Victoria Santos <sharmankamylle@gmail.com>
2024-01-02 12:36:54 -0300

    organizing js and formatting phone field

--- aula7/main.js
+++ aula7/main.js
@@ -64,6 +64,7 @@ var emailInput = document.querySelector('.emailInput');
 var passwordInput = document.querySelector('.passwordInput');
 
 addPersonBtn.addEventListener('click', () =>{
+
     //name verification
     if (/^[a-zA-ZÀ-ÖØ-öø-ÿ\s]+$/.test(nameInput.value.trim()) && nameInput.value.trim() !== '') {
         const newPerson = new Person();
@@ -75,6 +76,80 @@ addPersonBtn.addEventListener('click', () =>{
         else {
             alert('NAME ERROR. Please enter a valid name without the presence of numbers and null characters.Try again.');
         }
+
+
+
+    // cpf verification
+    var cpfOnlyNumbers = cpfInput.value.replace(/\./g, '').replace(/\-/g, ''); // regex
+    if (allDigitsEqual(cpfOnlyNumbers)) {
+        alert('Invalid CPF - All digits are equal.');
+        return;
+    } 
+        else if(cpfOnlyNumbers.length !== 11){
+            alert('Please fill in all the CPF fields.')
+            return;
+        }
+            else {
+                var cpfTenNumbers = cpfOnlyNumbers.substr(0, 10);
+                var cpfNineNumbers = cpfOnlyNumbers.substr(0, 9);
+                var cpfNineSum = 0;
+                var cpfTenSum = 0;
+                var cpfCounter = 10;
+
+                for (var i = 0; i < cpfNineNumbers.length; i++) {
+                    var number = parseInt(cpfNineNumbers.substr(i, 1));
+                    cpfNineSum += number * cpfCounter;
+                    cpfCounter--;
+                }
+
+                cpfCounter = 11;
+
+                for (var i = 0; i < cpfTenNumbers.length; i++) {
+                    var number = parseInt(cpfTenNumbers.substr(i, 1));
+                    cpfTenSum += number * cpfCounter;
+                    cpfCounter--;
+                }
+
+                var cpfResultModule1 = (cpfNineSum * 10) % 11;
+                cpfResultModule1 = cpfResultModule1 === 10 ? 0 : cpfResultModule1; 
+
+                var cpfResultModule2 = (cpfTenSum * 10) % 11;
+                cpfResultModule2 = cpfResultModule2 === 10 ? 0 : cpfResultModule2; 
+
+                if (cpfResultModule1 === parseInt(cpfOnlyNumbers.substr(9, 1)) && cpfResultModule2 === parseInt(cpfOnlyNumbers.substr(10, 1))) {
+                    const newPerson = new Person();
+                    newPerson.setCPF(cpfInput.value),
+                    people.push(newPerson);
+                    cpfInput.value = '';
+                    alert('Registered CPF');
+                } else {
+                    alert('Invalid CPF');
+                }
+            } 
+    
+    //phone validation
+    var phoneOnlyNumbers = phoneInput.value.replace(/\./g, '').replace(/\-/g, '');
+    if(phoneOnlyNumbers.length < 11 || phoneOnlyNumbers.length === ''){
+        alert('Please fill in all the phone fields.')
+        return;
+    }
+
+});
+
+
+function allDigitsEqual(cpf) {
+    const firstDigit = cpf.charAt(0);
+    return cpf.split('').every(digit => digit === firstDigit);
+}
+
+// cpf mask
+cpfInput.addEventListener('keypress', () => {
+    let cpfLength = cpfInput.value.length;
+    if (cpfLength === 3 || cpfLength === 7) {
+        cpfInput.value += '.';
+    } else if (cpfLength === 11) {
+        cpfInput.value += '-';
+    }
 });
 
 //phone mask
