45a4fa3cf1264e2d33dd0528a1353f52e21afa0f
Kamylle Victoria Santos <sharmankamylle@gmail.com>
2024-01-27 22:11:11 -0300

    adding features to the calculator. operator formatting.

--- project_calculator/main.js
+++ project_calculator/main.js
@@ -1,6 +1,11 @@
 const numericKeys = document.querySelectorAll('.num');
 const operatorsKeys = document.querySelectorAll('.item-operator');
 const resultItem = document.querySelector('.item-result');  
+const turnOnBtn = document.getElementById('on');
+const clearBtn = document.getElementById('ac');
+
+let signal = false
+
 const display = document.querySelector('.display');
 const maxLength = 17;
 
@@ -15,7 +20,36 @@ function addNumberDisplay(number){
 
 numericKeys.forEach((element) =>{
     element.addEventListener('click', (evt) => {
-        addNumberDisplay(evt.target.innerHTML)
+        signal = false;
 
+        if(display.innerHTML == '0'){
+            display.innerHTML = '';
+        }
+
+        addNumberDisplay(evt.target.innerHTML);
     })
+}) //exibir numeros no display
+
+
+operatorsKeys.forEach((element) => {
+    element.addEventListener('click', (evt) => {
+        if(!signal){
+            signal = true;
+
+            if(display.innerHTML == '0'){
+                display.innerHTML = '';
+            }
+            
+            if(evt.target.innerHTML == 'x'){
+                display.innerHTML += '*';
+            } else{
+                display.innerHTML += evt.target.innerHTML;
+            }
+        }
+    }) 
+})
+
+clearBtn.addEventListener('click', (evt) => {
+    signal = false;
+    display.innerHTML = ""
 })
